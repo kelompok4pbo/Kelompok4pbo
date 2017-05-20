c54767a30bc4e6d36d7e394fef3637c38961a2f9
izzantiwila <wilaizzanti@gmail.com>
2017-05-20 09:47:26 +0700

    method start

--- src/ektppbo/EktpPbo.java
+++ src/ektppbo/EktpPbo.java
@@ -15,7 +15,8 @@ public class EktpPbo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        tampilan tmp = new tampilan();
+        tmp.setVisible(true);
     }
     
 }
